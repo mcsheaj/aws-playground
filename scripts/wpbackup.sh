d52e29fd37d135eacb0fe33c1b34a68b987e2929
Joe McShea <joseph.mcshea@gmail.com>
2019-08-21 14:33:00 -0400

    Update wpbackup.sh

--- scripts/wpbackup.sh
+++ scripts/wpbackup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 SRC_DIR=
 PREFIX=
@@ -73,20 +73,32 @@ SUFFIX=$(date +"-%Y-%m-%d-%H-%M-%S-%Z")
 INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
 NAME=$(aws ec2 describe-tags --filters "Name=key,Values=Name" "Name=resource-id,Values=$INSTANCE_ID" --query "Tags[0].Value" --output text)
 FOLDER="${PREFIX}-${INTERVAL}"
-FILE_NAME="${FOLDER}${SUFFIX}.tar.gz"
+FILE_NAME="${PREFIX}${SUFFIX}.tar.gz"
+
+DB_FOLDER="db-${PREFIX}-${INTERVAL}"
+DB_FILE_NAME="db-${PREFIX}${SUFFIX}.sql.gz"
+DB_HOST=$(awk -F "=" '/DB_SERVER/ {print $2}' /root/.aws/bootstrap.properties)
+DB_USER=$(awk -F "=" '/DB_USER/ {print $2}' /root/.aws/bootstrap.properties)
+DB_PASS=$(awk -F "=" '/DB_PASSWORD/ {print $2}' /root/.aws/bootstrap.properties)
+
+DB=$(awk -F "=" '/DB_DATABASE/ {print $2}' /root/.aws/bootstrap.properties)
+if [ ${PREFIX} -ne "intellipoint" ]
+then
+    DB=$(awk -F "=" '/DB_DATABASE/ {print $2}' /root/.aws/bootstrap.properties)_${PREFIX}
+fi
 
 DST_BUCKET=$(awk -F "=" '/AWS_BUCKET/ {print $2}' /root/.aws/bootstrap.properties)
 
 # The backup server is the first server to come back from describe-instance with filter Name=$NAME
-BACKUP_SERVER=$(aws ec2 describe-instances --query "Reservations" --filters "Name=tag:Name,Values=${NAME}" | jq .[0].Instances[0].InstanceId -r)
+BACKUP_SERVER=$(aws ec2 describe-instances --query "Reservations" --filters "Name=tag:Name,Values=${NAME}" "Name=instance-state-name,Values=running" | jq .[0].Instances[0].InstanceId -r)
 
 cd $DIR_NAME
 
 if [ $INSTANCE_ID = $BACKUP_SERVER ]
 then
     tar -czf /tmp/${FILE_NAME} ${BASE_NAME} | true
-    aws s3 cp /tmp/${FILE_NAME} s3://${DST_BUCKET}/backup/${PREFIX}/${FILE_NAME}
-    rm /tmp/${FILE_NAME}
+    aws s3 cp /tmp/${FILE_NAME} s3://${DST_BUCKET}/backup/${FOLDER}/${FILE_NAME}
+    rm -rf /tmp/${FILE_NAME}
 
     BACKUP_FILES=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${FOLDER}/${PREFIX} --query "Contents[?contains(Key, '.tar.gz')] | sort_by(@, &LastModified)")
     NUM_FILES=$(echo ${BACKUP_FILES} | jq length)
@@ -103,8 +115,32 @@ then
             do
                 aws s3 rm s3://${DST_BUCKET}/${F}
             done
-        fi
-        
+        fi  
+
     fi
+
+    mysqldump --add-drop-table --host=$DB_HOST --user=$DB_USER --password=$DB_PASS $DB | gzip > /tmp/${DB_FILE_NAME}
+    aws s3 cp /tmp/${DB_FILE_NAME} s3://${DST_BUCKET}/backup/${DB_FOLDER}/${DB_FILE_NAME}
+    rm -rf /tmp/${DB_FILE_NAME}
+
+    BACKUP_FILES=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${DB_FOLDER}/db-${PREFIX} --query "Contents[?contains(Key, '.gz')] | sort_by(@, &LastModified)")
+    NUM_FILES=$(echo ${BACKUP_FILES} | jq length)
+
+    if [ ${NUM_FILES} -gt ${RETAIN} ]
+    then
+
+        let "EXCESS_FILES=$NUM_FILES - ${RETAIN}"
+        if [ ${EXCESS_FILES} -gt 0 ]
+        then
+            TODELETE=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${DB_FOLDER}/db-${PREFIX} --query "Contents[?contains(Key, '.gz')] | sort_by(@, &LastModified) | [0:${EXCESS_FILES}][].Key" --output text)
+
+            for F  in $TODELETE
+            do
+                aws s3 rm s3://${DST_BUCKET}/${F}
+            done
+        fi  
+
+    fi
+
 fi
 
