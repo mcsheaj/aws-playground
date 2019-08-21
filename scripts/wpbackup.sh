ed3d2e8df3fb01fd3f0ddfaea9768ad9f1d7e7b6
Joe McShea <joseph.mcshea@gmail.com>
2019-08-21 10:08:27 -0400

    Update wpbackup.sh

--- scripts/wpbackup.sh
+++ scripts/wpbackup.sh
@@ -2,6 +2,7 @@
 
 SRC_DIR=
 PREFIX=
+INTERVAL=
 RETAIN=4
 
 # Print help
@@ -12,11 +13,14 @@ function usage()
    echo "   --src-dir    - the directory to be archived (required)"
    echo "   --dst-bucket - the name of the bucket to which to copy the archive,"
    echo "                  will be copied to backup/${prefix}/{prefix}-{date}.tar.gz (required)"
-   echo "   --prefix     - the name of the backup folder and the prefix of the "
-   echo "                  backup file (required)"
+   echo "   --prefix     - the prefix of the backup folder and file (required)"
+   echo "   --interval   - the frequency of the backups, i.e. hour or daily, appended to "
+   echo "                  the prefix of the backup folder (required)"
    echo "   --retain     - the number of archives to retains (default is 4)"
-   echo "   --access-id  - the access key id of an IAM account you want to run under (optional)"
-   echo "   --secret-key - the secret key of the IAM account (optional)"
+   echo "   --access-id  - the access key id of an IAM account you want to run under (optional, "
+   echo "                  will use the environment variables or aws configure)"
+   echo "   --secret-key - the secret key of the IAM account (optional, "
+   echo "                  will use the environment variables or aws configure)"
    echo ""
 }
 
@@ -31,6 +35,12 @@ while [ "$1" != "" ]; do
                         ;;
         --prefix)       shift
                         PREFIX=$1
+                        ;;
+        --prefix)       shift
+                        PREFIX=$1
+                        ;;
+        -interval)      shift
+                        INTERVAL=$1
                         ;;
         --retain)       shift
                         RETAIN=$1
@@ -49,7 +59,7 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [[ -z $SRC_DIR || -z $PREFIX ]]; then
+if [[ -z $SRC_DIR || -z $PREFIX  || -z $INTERVAL ]]; then
     usage
     exit 1
 fi
@@ -62,7 +72,8 @@ export AWS_DEFAULT_REGION=$(curl http://169.254.169.254/latest/dynamic/instance-
 SUFFIX=$(date +"-%Y-%m-%d-%H-%M-%S-%Z")
 INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
 NAME=$(aws ec2 describe-tags --filters "Name=key,Values=Name" "Name=resource-id,Values=$INSTANCE_ID" --query "Tags[0].Value" --output text)
-FILE_NAME="${PREFIX}${SUFFIX}.tar.gz"
+FOLDER="${PREFIX}-${INTERVAL}"
+FILE_NAME="${FOLDER}${SUFFIX}.tar.gz"
 
 DST_BUCKET=$(awk -F "=" '/AWS_BUCKET/ {print $2}' /root/.aws/bootstrap.properties)
 
@@ -77,7 +88,7 @@ then
     aws s3 cp /tmp/${FILE_NAME} s3://${DST_BUCKET}/backup/${PREFIX}/${FILE_NAME}
     rm /tmp/${FILE_NAME}
 
-    BACKUP_FILES=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${PREFIX}/${PREFIX} --query "Contents[?contains(Key, '.tar.gz')] | sort_by(@, &LastModified)")
+    BACKUP_FILES=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${FOLDER}/${PREFIX} --query "Contents[?contains(Key, '.tar.gz')] | sort_by(@, &LastModified)")
     NUM_FILES=$(echo ${BACKUP_FILES} | jq length)
 
     if [ ${NUM_FILES} -gt ${RETAIN} ]
@@ -86,7 +97,7 @@ then
         let "EXCESS_FILES=$NUM_FILES - ${RETAIN}"
         if [ ${EXCESS_FILES} -gt 0 ]
         then
-            TODELETE=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${PREFIX}/${PREFIX} --query "Contents[?contains(Key, '.tar.gz')] | sort_by(@, &LastModified) | [0:${EXCESS_FILES}][].Key" --output text)
+            TODELETE=$(aws s3api list-objects --bucket ${DST_BUCKET} --prefix backup/${FOLDER}/${PREFIX} --query "Contents[?contains(Key, '.tar.gz')] | sort_by(@, &LastModified) | [0:${EXCESS_FILES}][].Key" --output text)
 
             for F  in $TODELETE
             do
