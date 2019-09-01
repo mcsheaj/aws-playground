36e52d04c15cc0821fa7677843e03e5bf813342a
mcsheaj <joseph.mcshea@gmail.com>
2019-09-01 13:43:21 -0400

    .

---
+++ bootstrap/bootstrap-nat-linux2.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -xe
+
+# Install jq
+yum -y install jq
+
+cd /tmp
+
+# Populate some variables from meta-data
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
+# Populate some variables from tags (need jq installed first)
+NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=Name" "Name=resource-id,Values=$INSTANCE_ID" | jq .Tags[0].Value -r)
+STACK_NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=StackName" "Name=resource-id,Values=$INSTANCE_ID" | jq .Tags[0].Value -r)
+
+# Create user accounts for administrators
+if ! [ -z "$ADMIN_GROUP" ]
+then
+    yum -y install git
+    wget --no-cache https://raw.githubusercontent.com/mcsheaj/aws-ec2-ssh/master/install.sh
+    chmod 755 install.sh
+    ./install.sh -i $ADMIN_GROUP -s $ADMIN_GROUP
+else 
+    echo "No ADMIN_GROUP specified, skipping aws-ect-ssh configuration"
+fi
+
+# Update the motd banner
+if ! [ -z "$MOTD_BANNER" ]
+then
+    wget --no-cache -O /etc/update-motd.d/30-banner $MOTD_BANNER
+    update-motd --force
+    update-motd --disable
+else 
+    echo "No MOTD_BANNER specified, skipping motd configuration"
+fi
+
+# Remove the ec2-user
+userdel -f ec2-user
+
+# Turn on IPV4 forwarding
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+VPC_CIDR=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$/vpc-ipv4-cidr-block)
+
+# Enable nat in iptables for our VPC CIDDR
+iptables -t nat -A POSTROUTING -o eth0 -s ${VPC_CIDR} -j MASQUERADE
+
+# Download the script to update the default routes on the private networks and run it
+wget --no-cache https://raw.githubusercontent.com/mcsheaj/aws-playground/master/scripts/aws-auto-healing-nat.sh
+mv aws-auto-healing-nat.sh /sbin
+chmod 700 /sbin/aws-auto-healing-nat.sh
+/sbin/aws-auto-healing-nat.sh
+
+# Re-enable nat and reset the private route tables on boot
+echo "iptables -t nat -A POSTROUTING -o eth0 -s ${VPC_CIDR} -j MASQUERADE" >> /etc/rc.d/rc.local
+echo "/sbin/aws-auto-healing-nat.sh" >> /etc/rc.d/rc.local
+chmod 700 /etc/rc.d/rc.local
+
+# Update the instance name to include the stack name
+if [[ $NAME != *-$STACK_NAME ]]
+then
+    NEW_NAME="$NAME-$STACK_NAME"
+    aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$NEW_NAME --region $REGION
+else
+    NEW_NAME=$NAME
+fi
+
+# Run system updates
+yum -y update
--- scripts/aws-auto-healing-nat.sh
+++ scripts/aws-auto-healing-nat.sh
@@ -2,10 +2,10 @@
 
 # This script is to set a default route for route tables that need NAT in the same VPC.
 
-MAC_ADDRESS=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/`
-VPC_ID=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC_ADDRESS}/vpc-id`
-INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
-REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+MAC_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
+VPC_ID=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC_ADDRESS}/vpc-id)
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 TAG_KEY="Network"
 TAG_VALUE="Private"
 
@@ -13,20 +13,20 @@ TAG_VALUE="Private"
 function usage()
 {
    echo ""
-   echo "$0 [options]"
+   echo "${0} [options]"
    echo "   --tag-key - route table tag key to indicate if it is a private subnet (default: $TAG_KEY)"
    echo "   --tag-value - route table tag value to indicate if it is a private subnet (default: $TAG_VALUE)"
    echo ""
 }
 
 # Get options
-while [ "$1" != "" ]; do
-    case $1 in
+while [ "${1}" != "" ]; do
+    case ${1} in
         --tag-key)      shift
-                        $TAG_KEY=$1
+                        ${TAG_KEY}=$1
                         ;;
         --tag-value)    shift
-                        $TAG_VALUE=$1
+                        ${TAG_VALUE}=$1
                         ;;
         *)              usage
                         exit
@@ -36,21 +36,21 @@ while [ "$1" != "" ]; do
 done
 
 # Determine route tables that need to use NAT for the same VPC
-ROUTE_TABLES=`aws ec2 describe-route-tables --filters "Name=tag:$TAG_KEY,Values=$TAG_VALUE" --region $REGION --output text | grep $VPC_ID | awk '{print $3}'`
+ROUTE_TABLES=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:${TAG_KEY},Values=${TAG_VALUE}" --region ${REGION} --output json | jq .RouteTables[].RouteTableId -r)
 
 for ROUTE_TABLE  in $ROUTE_TABLES
 do
-   TARGET=`aws ec2 describe-route-tables --filters "Name=route-table-id,Values=$ROUTE_TABLE" --region $REGION --output text | grep "0.0.0.0/0" | awk '{print $3}'`
+   TARGET=$(aws ec2 describe-route-tables --filters "Name=route-table-id,Values=${ROUTE_TABLE}" --region ${REGION} | jq '.RouteTables[].Routes[] | select(.DestinationCidrBlock == "0.0.0.0/0")' | jq .InstanceId -r)
 
-   echo "Checking $ROUTE_TABLE"
+   echo "Checking ${ROUTE_TABLE}"
    if [ "$TARGET" = "" ]; then
       # Create default route 
-      echo "No default route is detected. Creating default route for $ROUTE_TABLE"
-      aws ec2 create-route --route-table-id $ROUTE_TABLE --destination-cidr-block 0.0.0.0/0 --instance-id $INSTANCE_ID --region $REGION
-   elif [ "$TARGET" != "$INSTANCE_ID" ]; then
+      echo "No default route is detected. Creating default route for ${ROUTE_TABLE}"
+      aws ec2 create-route --route-table-id $ROUTE_TABLE --destination-cidr-block 0.0.0.0/0 --instance-id ${INSTANCE_ID} --region ${REGION}
+   elif [ "${TARGET}" != "${INSTANCE_ID}" ]; then
       # Replace default route
-      echo "Default route is set to $TARGET. Replacing default route to $INSTANCE_ID"
-      aws ec2 replace-route --route-table-id $ROUTE_TABLE --destination-cidr-block 0.0.0.0/0 --instance-id $INSTANCE_ID --region $REGION
+      echo "Default route is set to ${TARGET}. Replacing default route to ${INSTANCE_ID}"
+      aws ec2 replace-route --route-table-id ${ROUTE_TABLE} --destination-cidr-block 0.0.0.0/0 --instance-id ${INSTANCE_ID} --region ${REGION}
    else
       echo "No change is required"
    fi
