5da7867a0c346b824e4af123168a5b747931a657
mcsheaj <joseph.mcshea@gmail.com>
2019-09-01 22:10:32 -0400

    .

--- bootstrap/bootstrap-bastion.sh
+++ bootstrap/bootstrap-bastion.sh
@@ -6,31 +6,29 @@ yum -y install jq
 cd /tmp
 
 # Populate some variables from meta-data
-INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
-REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
-ADMIN_GROUP=$1
-MOTD_BANNER=$2
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 
 # Populate some variables from tags (need jq installed first)
-NAME=`aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=Name" "Name=resource-id,Values=$INSTANCE_ID" | jq .Tags[0].Value -r`
-STACK_NAME=`aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=StackName" "Name=resource-id,Values=$INSTANCE_ID" | jq .Tags[0].Value -r`
+NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=Name" "Name=resource-id,Values=${INSTANCE_ID}" | jq .Tags[0].Value -r)
+STACK_NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=StackName" "Name=resource-id,Values=${INSTANCE_ID}" | jq .Tags[0].Value -r)
 
 # Update the instance name to include the stack name
-if [[ $NAME != *-$STACK_NAME ]]
+if [[ ${NAME} != *-${STACK_NAME} ]]
 then
-    NEW_NAME="$NAME-$STACK_NAME"
-    aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$NEW_NAME --region $REGION
+    NEW_NAME="${NAME}-${STACK_NAME}"
+    aws ec2 create-tags --resources ${INSTANCE_ID} --tags Key=Name,Value=$NEW_NAME --region $REGION
 else
-    NEW_NAME=$NAME
+    NEW_NAME=${NAME}
 fi
 
 # Create user accounts for administrators
-if ! [ -z "$ADMIN_GROUP" ]
+if ! [ -z "${ADMIN_GROUP}" ]
 then
     yum -y install git
     wget --no-cache https://raw.githubusercontent.com/mcsheaj/aws-ec2-ssh/master/install.sh
     chmod 755 install.sh
-    ./install.sh -i $ADMIN_GROUP -s $ADMIN_GROUP
+    ./install.sh -i ${ADMIN_GROUP} -s ${ADMIN_GROUP}
 else 
     echo "No ADMIN_GROUP specified, skipping aws-ect-ssh configuration"
 fi
@@ -39,9 +37,9 @@ fi
 yum -y update
 
 # Update the motd banner
-if ! [ -z "$MOTD_BANNER" ]
+if ! [ -z "${MOTD_BANNER}" ]
 then
-    wget --no-cache -O /etc/update-motd.d/30-banner $MOTD_BANNER
+    wget --no-cache -O /etc/update-motd.d/30-banner ${MOTD_BANNER}
     update-motd --force
     update-motd --disable
 else 
