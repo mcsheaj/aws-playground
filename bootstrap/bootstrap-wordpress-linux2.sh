c98f52f1d03efe05d427dbb02da2605dfb256f68
Joe McShea <joseph.mcshea@gmail.com>
2021-06-01 16:51:24 -0400

    Update bootstrap-wordpress-linux2.sh

--- bootstrap/bootstrap-wordpress-linux2.sh
+++ bootstrap/bootstrap-wordpress-linux2.sh
@@ -160,16 +160,23 @@ rm -rf joemcshea.intellipointsolutions.com
 
 # Configure cache expiry for static content
 cat << EOF > /var/www/joemcshea.intellipointsolutions.com/html/.htaccess
+
 # BEGIN WordPress
+# The directives (lines) between "BEGIN WordPress" and "END WordPress" are
+# dynamically generated, and should only be modified via WordPress filters.
+# Any changes to the directives between these markers will be overwritten.
 <IfModule mod_rewrite.c>
 RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule . /index.php [L]
 </IfModule>
+
 # END WordPress
+
 <IfModule mod_expires.c>
 ExpiresActive On
 # Images
